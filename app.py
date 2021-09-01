8c43ffd3516c96674f0920d636cdd3de9053b7cd
Paulina Rojas <88862242+projas89@users.noreply.github.com>
2021-09-01 07:25:23 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+  
+# from app import catfact
+
+# def test_catfact():
+#     fact = catfact().json
+#     print(fact)
+
+#     assert fact['length'] > 0
+
+def test_catfact():
+    pass
+
+test_catfact()
+© 2021 GitHub, Inc.
